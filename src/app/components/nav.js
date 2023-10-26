c5daefbf5aceaf7c76c5f802f696a2aabc73b057
Ali-404 <abdellali.aitaddi@gmail.com>
2023-10-26 18:29:32 +0100

    add nav bar

--- src/app/components/nav.js
+++ src/app/components/nav.js
@@ -1,8 +1,21 @@
+import styles from '../styles/nav.css'
+
 
 export default function Nav() {
   return (
-    <div>
-        <h1>test</h1>
+    <div className='container'>
+        <img src='ali.png'  />
+
+        <div className='links'>
+
+            <a>Home</a>
+            <a>Skills</a>
+            <a>Projects</a>
+            <a>Education</a>
+            <a>Contact</a>
+
+        </div>
+
     </div>
   )
 }
